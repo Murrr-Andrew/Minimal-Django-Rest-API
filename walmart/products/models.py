266633a1106bb555dfabce59d054bb9767405a4a
Andrew Murrow <murrr.andrew@gmail.com>
2023-02-09 16:02:54 +0200

    Django-Rest: CRUD

--- walmart/products/models.py
+++ walmart/products/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
