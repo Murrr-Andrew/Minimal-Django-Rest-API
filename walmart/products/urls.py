51249c14b70b8fde46cd7a34baea57147704e6bb
Andrew Murrow <murrr.andrew@gmail.com>
2023-02-10 22:59:57 +0200

    Django-Rest: Auth

--- walmart/products/urls.py
+++ walmart/products/urls.py
@@ -4,5 +4,5 @@ from .views import ProductList, ProductDetail
 
 urlpatterns = [
     path('products/', ProductList.as_view(), name='product-list'),
-    path('products/int:pk/', ProductDetail.as_view(), name='product-detail'),
+    path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
 ]
--- walmart/products/views.py
+++ walmart/products/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -6,8 +8,12 @@ from .serializers import ProductSerializer
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = (BasicAuthentication, )
+    permission_classes = (IsAuthenticated, )
 
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = (BasicAuthentication, )
+    permission_classes = (IsAuthenticated, )
